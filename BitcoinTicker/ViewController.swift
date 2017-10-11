ec00a6e13bba9b7023c52237093ee45dbf0299c9
Chuabacca <jonathan.chua@dollarshaveclub.com>
2017-10-11 15:33:08 -0700

    Started Bitcoin ticker project.

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
@@ -18,21 +18,57 @@ class ViewController: UIViewController {
     @IBOutlet weak var bitcoinPriceLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
     
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
     }
 
     
     //TODO: Place your 3 UIPickerView delegate methods here
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        finalURL = baseURL + currencyArray[row]
+        print(finalURL)
+    }
     
     
-
+    //Use Codable
+    
+    struct BitcoinData: Codable {
+        let averages: [String: Averages]
+        struct Averages : Codable {
+            let day: String
+        }
+    }
     
     
+    
+    HTTP.GET("http://localhost:8080/bar") { response in
+    if let error = response.error {
+    print("got an error: \(error)")
+    return
+    }
+    do {
+    let decoder = JSONDecoder()
+    let resp = try decoder.decode(Response.self, from: response.data)
+    print("completed: \(resp.status)")
+    } catch let error {
+    print("decode json error: \(error)")
+    }
+    }
     
 //    
 //    //MARK: - Networking
